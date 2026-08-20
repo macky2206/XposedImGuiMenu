258726da687e18a8e34e83e972e4f3af7b4a54b7
macky2206 <zakiandrazaki123@gmail.com>
2026-08-20 13:03:40 +0700

    cycle18

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -34,27 +34,12 @@ EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
     return old_eglSwapBuffers(dpy, surface);
 }
 
-void *hack_thread(void *arg) {
-    // 1. Cast the passed argument back to jobject
-    jobject globalContext = (jobject)arg;
-
-    // 2. Wait for IL2CPP to load into memory
+void *hack_thread(void *) {
     do {
         sleep(1);
         g_il2cppBaseMap = KittyMemory::getElfBaseMap("libil2cpp.so");
     } while (!g_il2cppBaseMap.isValid());
 
-    // 3. Attach this background thread to the JVM to get a valid JNIEnv
-    JNIEnv *env;
-    jvm->AttachCurrentThread(&env, nullptr);
-
-    // 4. NOW initialize BNM (it will succeed because libil2cpp is in memory)
-    BNM::Loading::TryLoadByJNI(env, globalContext);
-    
-    // 5. Clean up the global reference to prevent memory leaks
-    env->DeleteGlobalRef(globalContext);
-
-    // 6. Original logic: Wait for Unity
     do {
         sleep(1);
         unityMaps = ElfScanner::createWithPath("libunity.so");
@@ -77,27 +62,42 @@ void *hack_thread(void *arg) {
 
     LOGI("Menu finished loading");
 
-    // 7. Detach thread from JVM before exiting
-    jvm->DetachCurrentThread();
-
     return nullptr;
 }
+
+void OnBNMLoaded() {
+    BNM_LOG_INFO(std::string(50, '=').c_str());
+    BNM_LOG_INFO("BNM has been loaded");
+
+    Features::freeShop::initHook();
+    // Features::SpeedHack::initHook();
+    // Features::SkillsHack::initHook();
+    // Features::LowestWave::initHook();
+    // Features::Equipment::initHook();
+    Features::set0Prices::initHook();
+    // Features::RuneLevel::initHook();
+    // Features::Expedition::initHook();
+
+    BNM_LOG_INFO(std::string(50, '=').c_str());
+}
+
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env;
     jvm = vm; // Store the JavaVM globally
     vm->GetEnv((void **) &env, JNI_VERSION_1_6);
+    
+    BNM::Loading::AddOnLoadedEvent(OnBNMLoaded);
+    BNM::Loading::TryLoadByJNI(env);
 
     return JNI_VERSION_1_6;
 }
 
+// 2. Added JNI Bridge: This catches the Activity from Inject.java
 extern "C"
 JNIEXPORT void JNICALL
 Java_org_modfs_xposedmenu_Inject_startModMenu(JNIEnv *env, jclass clazz, jobject activityContext) {
-    
-    // Note: BNM::Loading::TryLoadByJNI has been moved to hack_thread.
 
-    // 1. Resolve UnityPlayer class using the target app's ClassLoader
     jclass activityClass = env->GetObjectClass(activityContext);
     jmethodID getClassLoaderMethod = env->GetMethodID(activityClass, "getClassLoader", "()Ljava/lang/ClassLoader;");
     jobject targetClassLoader = env->CallObjectMethod(activityContext, getClassLoaderMethod);
@@ -116,13 +116,9 @@ Java_org_modfs_xposedmenu_Inject_startModMenu(JNIEnv *env, jclass clazz, jobject
         LOGE("Failed to find com.unity3d.player.UnityPlayer using target ClassLoader!");
     }
 
-    // 2. Create a Global Reference of the Activity context to pass to the new thread
-    jobject globalContext = env->NewGlobalRef(activityContext);
-
-    // 3. Start the hack thread and pass the globalContext as the 4th argument
     int ret;
     pthread_t ntid;
-    if ((ret = pthread_create(&ntid, nullptr, hack_thread, (void*)globalContext))) {
+    if ((ret = pthread_create(&ntid, nullptr, hack_thread, nullptr))) {
         LOGE("can't create thread: %s\n", strerror(ret));
     }
 }
